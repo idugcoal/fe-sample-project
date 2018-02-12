3fcaf2ba5f9c38fb28d576b045a4347361b1def6
Doug Cole <idugcoal@gmail.com>
2018-02-12 02:49:30 -0800

    Adjusts styling of Shop and Cart components

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -6,8 +6,10 @@ import styles from './CartItem.css';
 const CartItem = ({ filename, price, name, onClick }) => (
   <div className={styles.cart_item} key={filename}>
     <img src={`../../assets/images/${filename}`} alt={`${name}`} />
-    <div className={styles.name}>{name}</div>
-    <div className={styles.price}>{`$${(price / 100).toFixed(2)}`}</div>
+    <div className={styles.container}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.price}>{`$${(price / 100).toFixed(2)}`}</div>
+    </div>
     <div className={styles.remove} onClick={() => onClick({ filename, price, name })} />
   </div>
 );
