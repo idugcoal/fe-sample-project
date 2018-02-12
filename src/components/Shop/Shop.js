afd4e8ac732ab1d738bfd72f4fbe125732a13f5b
Doug Cole <idugcoal@gmail.com>
2018-02-12 12:07:57 -0800

    Updates CartItem styling

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Title from '../Title/Title';
 import ShopItem from '../ShopItem/ShopItem';
 import styles from './Shop.css';
-
-// import { getProducts } from '../../utils';
 
 export default class Shop extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cart: [],
       products: this.props.products,
     };
     this.renderProducts = this.renderProducts.bind(this);
@@ -37,3 +35,8 @@ export default class Shop extends Component {
     );
   }
 }
+
+Shop.propTypes = {
+  onClick: PropTypes.func,
+  products: PropTypes.array,
+};
