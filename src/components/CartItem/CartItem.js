7494bf0158bf79f5c6e6ce15c2f7d45c86a007f1
Doug Cole <idugcoal@gmail.com>
2018-02-12 19:30:53 -0800

    Adjusts path to images in CartItem and ShopItem

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -5,7 +5,7 @@ import styles from './CartItem.css';
 
 const CartItem = ({ filename, price, name, onClick }) => (
   <div className={styles.cart_item} key={filename}>
-    <img src={`../../../assets/images/${filename}`} alt={`${name}`} />
+    <img src={`./assets/images/${filename}`} alt={`${name}`} />
     <div className={styles.container}>
       <div className={styles.name}>{name}</div>
       <div className={styles.price}>{`$${(price / 100).toFixed(2)}`}</div>
--- src/components/ShopItem/ShopItem.js
+++ src/components/ShopItem/ShopItem.js
@@ -5,7 +5,7 @@ import styles from './ShopItem.css';
 
 const ShopItem = ({ filename, price, name, onClick }) => (
   <div className={styles.item}>
-    <img src={`../../../assets/images/${filename}`} alt={`${name}`} />
+    <img src={`./assets/images/${filename}`} alt={`${name}`} />
     <div className={styles.name}>{name}</div>
     <div className={styles.price}>{`$${(price / 100).toFixed(2)}`}</div>
     <Button
